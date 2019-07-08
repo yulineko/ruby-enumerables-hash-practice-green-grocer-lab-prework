5519b202c318052a515cd38b3d664c3c9fc084f9
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-08 22:11:00 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,21 +1,40 @@
 def consolidate_cart(cart)
   # code here
-  cons_cart = {}
+  consolidated_cart = {}
   cart.each do |item|
-    item.each do |key, value|
-      if cons_cart[key]
-        cons_cart[key][:count] += 1
-      else
-        updated_cart[key] = value
-        updated_cart[key][:count] = 1
-      end
+    if consolidated_cart[item.keys[0]]
+      consolidated_cart[item.keys[0]][:count] += 1
+    else
+      consolidated_cart[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
     end
   end
-  cons_cart
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= [:num]
+        name = "#{coupon[:item]} W/COUPON"
+        if cart[name]
+          cart[name][:count] += coupon[:num]
+        else 
+          cart[name] = {
+            count: coupon[:num],
+            price: coupon[:cost] / coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
